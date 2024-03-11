f7b896965934082aacff0e55cee7a1b4c66fc6e6
Sandeeb Shyam <93623711+SandeebShyam@users.noreply.github.com>
2024-03-11 14:07:13 +0200

    Add files via upload

---
+++ Robot_Code/constants.h
@@ -0,0 +1,12 @@
+#define hipLOffset 90
+#define kneeLOffset 90
+#define ankleLOffset 90
+#define hipROffset 90
+#define kneeROffset 90
+#define ankleROffset 90
+
+#define l1 5
+#define l2 5.7
+
+#define stepClearance 1
+#define stepHeight 10
